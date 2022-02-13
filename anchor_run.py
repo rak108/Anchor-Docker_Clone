12782c8037eeb9134f35c8e1d918af6661ccadbd
Inbasekaran <inba2002.p@gmail.com>
2022-02-13 21:19:04 +0530

    Done with level2 mount namespace

--- anchor_run.py
+++ anchor_run.py
@@ -31,6 +31,9 @@ import traceback
 
 import linux
 
+import stat
+
+
 def _get_image_path(image_name, image_dir, image_suffix='tar'):
     """
     Function to obtain path to image
@@ -44,6 +47,7 @@ def _get_image_path(image_name, image_dir, image_suffix='tar'):
 
     """
     return os.path.join(image_dir, os.extsep.join([image_name, image_suffix]))
+
 
 def _get_container_path(container_id, container_dir, *subdir_names):
     """
@@ -59,6 +63,7 @@ def _get_container_path(container_id, container_dir, *subdir_names):
     """
     return os.path.join(container_dir, container_id, *subdir_names)
 
+
 def create_container_root(image_name, image_dir, container_id, container_dir):
     """
     Create a container root by extracting an image into a new directory
@@ -68,7 +73,7 @@ def create_container_root(image_name, image_dir, container_id, container_dir):
     :param container_id: the unique container id
     :param container_dir: the base directory of newly generated container
                           directories
-    
+
     :return: full container path created
 
     """
@@ -88,14 +93,31 @@ def create_container_root(image_name, image_dir, container_id, container_dir):
 
     return container_root
 
+
 @click.group()
 def cli():
     pass
 
+
+def makedev(dev_path):
+    for i, dev in enumerate(['stdin', 'stdout', 'stderr']):
+        os.symlink('/proc/self/fd/%d' % i, os.path.join(dev_path, dev))
+    os.symlink('/proc/self/fd', os.path.join(dev_path, 'fd'))
+    # Add extra devices (null, zero, random, urandom, console ...)
+    DEVICES = {'null': (stat.S_IFCHR, 1, 3), 'zero': (stat.S_IFCHR, 1, 5),
+               'random': (stat.S_IFCHR, 1, 8), 'urandom': (stat.S_IFCHR, 1, 9),
+               'console': (stat.S_IFCHR, 136, 1), 'tty': (stat.S_IFCHR, 5, 0),
+               'full': (stat.S_IFCHR, 1, 7)}
+    for device, (dev_type, major, minor) in DEVICES.items():
+        # mode = 0o666, allows read and write file operations within the created directory
+        os.mknod(os.path.join(dev_path, device),
+                 0o666 | dev_type, os.makedev(major, minor))
+
+
 def contain(command, image_name, image_dir, container_id, container_dir):
     """
     Contain function that is used to actually create the contained space.
-  
+
     :param command: Command passed while running the file
     :param image_name: Physical file name of Image
     :param image_dir: Directory path of image 
@@ -103,6 +125,22 @@ def contain(command, image_name, image_dir, container_id, container_dir):
     :param container_dir: Directory path of container to be made
 
     """
+    try:
+        # create a new mount namespace
+        linux.unshare(linux.CLONE_NEWNS)
+        # CLONE_NEWNS provides the child with a new mount namespace (requires ADMIN capability)
+    except RuntimeError as e:
+        if getattr(e, 'args', '') == (1, 'Operation not permitted'):
+            print('Error: Use of CLONE_NEWNS with unshare(2) requires the '
+                  'CAP_SYS_ADMIN capability (i.e. you probably want to retry '
+                  'this with sudo)')
+        raise e
+    # MS_PRIVATE makes the mount private
+    # MS_REC creates a recursive bind mount
+
+    # NOTE: MS_REC is added along with MS_PRIVATE to change the propagation type of all of the mounts in a subtree
+    linux.mount(None, '/', None, linux.MS_PRIVATE | linux.MS_REC, None)
+
     new_root = create_container_root(
         image_name, image_dir, container_id, container_dir)
     print('Created a new root fs for our container: {}'.format(new_root))
@@ -110,12 +148,12 @@ def contain(command, image_name, image_dir, container_id, container_dir):
     # In order to actually access the configurations of the container being created, we require these 3 pseudo-filesystems
     # proc: information about the real runtime system configurations
     linux.mount('proc', os.path.join(new_root, 'proc'), 'proc', 0, '')
-    # sys: information about various kernel subsystems, hardware devices, and associated device drivers 
+    # sys: information about various kernel subsystems, hardware devices, and associated device drivers
     linux.mount('sysfs', os.path.join(new_root, 'sys'), 'sysfs', 0, '')
-    # tmp: a temporary file storage; acts similar to RAM 
-    # NOSUID: prevents the 'suid' bit on executables from taking effect, and thus essentially allows anyone other than 
+    # tmp: a temporary file storage; acts similar to RAM
+    # NOSUID: prevents the 'suid' bit on executables from taking effect, and thus essentially allows anyone other than
     # the executables owner to also run the executable;
-    # STRICTATIME: updates the access time of the files every time they are accessed 
+    # STRICTATIME: updates the access time of the files every time they are accessed
     linux.mount('tmpfs', os.path.join(new_root, 'dev'), 'tmpfs',
                 linux.MS_NOSUID | linux.MS_STRICTATIME, 'mode=755')
 
@@ -124,8 +162,8 @@ def contain(command, image_name, image_dir, container_id, container_dir):
     if not os.path.exists(devpts_path):
         os.makedirs(devpts_path)
         linux.mount('devpts', devpts_path, 'devpts', 0, '')
-    for i, dev in enumerate(['stdin', 'stdout', 'stderr']):
-        os.symlink('/proc/self/fd/%d' % i, os.path.join(new_root, 'dev', dev))
+
+    makedev(os.path.join(new_root, 'dev'))
 
     # Change root directory to the newly created one
     os.chroot(new_root)
@@ -134,6 +172,7 @@ def contain(command, image_name, image_dir, container_id, container_dir):
 
     os.execvp(command[0], command)
 
+
 @cli.command(context_settings=dict(ignore_unknown_options=True,))
 @click.option('--image-name', '-i', help='Image name', default='ubuntu-export')
 @click.option('--image-dir', help='Images directory',
@@ -141,11 +180,10 @@ def contain(command, image_name, image_dir, container_id, container_dir):
 @click.option('--container-dir', help='Containers directory',
               default='./build/containers')
 @click.argument('Command', required=True, nargs=-1)
-
 def run(image_name, image_dir, container_dir, command):
     """
     Run function that is called via the 'run' arugment in the command-line command
-   
+
     :param image_name: Physical file name of Image
     :param image_dir: Directory path of image 
     :param container_dir: Directory path of container to be made
